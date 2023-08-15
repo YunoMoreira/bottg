6adcefb58580a92e9ee8dc889ec0494ec03b150d
yunomoreira <gucciomoreira@yandex.ru>
2023-08-16 00:50:51 +0300

    Эхо бот

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ bot = telebot.TeleBot(conf.BOT_TOKEN)
 
 @bot.message_handler (content_types=["text"])
 def repeat_all_message (message):
-    bot.send_message(message.chat.id, message.text)
+    reply_text = "Привет"
+    bot.send_message(message.chat.id, reply_text + ", " + message.from_user.first_name)
 
 if __name__ == '__main__':
     bot.infinity_polling()
+
